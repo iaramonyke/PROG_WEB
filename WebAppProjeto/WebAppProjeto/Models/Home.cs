ee8d1fe646dc2b39bb919ada9bb31939dcde13b1
Iara Monyke <i.monyke@academico.ifrn.edu.br>
2023-05-16 15:36:36 -0300

    finalização home

---
+++ WebAppProjeto/WebAppProjeto/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppProjeto.Models
+{
+    public class Home
+    {
+        public IOrderedQueryable<Fabricante> fabricantes;
+        public IOrderedQueryable<Categoria> categorias;
+
+    }
+}
